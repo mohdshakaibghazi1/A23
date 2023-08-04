d19df7ada35408b89bb728ec1ae29d96a2091007
Mohd Shakaib Ghazi <mohd.shakaibghazi@mphasis.com>
2023-08-04 15:29:44 +0530

    A23

---
+++ MainApp/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Services;
+using System.Text;
+using System.Threading.Tasks;
+using MySharedLibrary;
+
+namespace MainApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("Enter User Name");
+            string str=Console.ReadLine();
+
+            if (Utilities.IsValidName(str))
+            {
+                Console.WriteLine($"{str} is a valid name.");
+            }
+            else
+            {
+                Console.WriteLine($"{str} is not a valid name.");
+            }
+            Console.WriteLine("Enter Passwrd");
+            Console.WriteLine("password should contain one upper case lowers case and digit");
+
+            string password = Console.ReadLine();
+            if (Utilities.IsValidPassword(password))
+            {
+                Console.WriteLine("Password is valid.");
+            }
+            else
+            {
+                Console.WriteLine("Password is not valid.");
+            }
+        }
+    }
+}
---
+++ MySharedLibrary/MySharedLibrary/Class1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace MySharedLibrary
+{
+    public class Utilities
+    {
+
+        public static bool IsValidName(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                return false;
+
+            return name.Length >= 3;
+        }
+
+        public static bool IsValidPassword(string password)
+        {
+            if (string.IsNullOrWhiteSpace(password))
+                return false;
+            bool hasUpperCase = false;
+            bool hasLowerCase = false;
+            bool hasDigit = false;
+
+            foreach (char c in password)
+            {
+                if (char.IsUpper(c))
+                    hasUpperCase = true;
+                else if (char.IsLower(c))
+                    hasLowerCase = true;
+                else if (char.IsDigit(c))
+                    hasDigit = true;
+
+                if (hasUpperCase && hasLowerCase && hasDigit)
+                    break;
+            }
+
+            return password.Length >= 8 && hasUpperCase && hasLowerCase && hasDigit;
+        }
+
+        public static void LogMessage(string message)
+        {
+            Console.WriteLine("[LOG] " + message);
+        }
+        public static string EncryptPassword(string password)
+        {
+
+            return password;
+        }
+    }
+}
